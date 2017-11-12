b085fe1925eb955e117e7457ecdc152120353e32
sgajjar97 <sgajjar97@gmail.com>
2017-11-11 16:34:14 -0800

    DLineModel (Done)

--- src/com/CS151FinalProject/DModel/DLineModel.java
+++ src/com/CS151FinalProject/DModel/DLineModel.java
@@ -1,5 +1,15 @@
 package com.CS151FinalProject.DModel;
 
-public class DLineModel {
+public class DLineModel extends DShapeModel {
+	
+	public DLineModel(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		
+	}
 
+	public DLineModel()
+	{
+		super();
+	}
+	
 }
