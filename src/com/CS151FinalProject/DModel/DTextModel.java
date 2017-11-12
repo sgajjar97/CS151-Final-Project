3c98542573d0a7e5d2b48a282f8df2de37dde137
Shail Gajjar <sgajjar97@gmail.com>
2017-11-11 16:39:33 -0800

    DTextModel (Done)

--- src/com/CS151FinalProject/DModel/DTextModel.java
+++ src/com/CS151FinalProject/DModel/DTextModel.java
@@ -1,5 +1,43 @@
 package com.CS151FinalProject.DModel;
 
-public class DTextModel {
+public class DTextModel extends DShapeModel {
+	
+	String text;
+	String fontType;
+	
+	public DTextModel(int x, int y, int width, int height, String text, String fontType)
+	{
+		super(x,y,width,height);
+		this.text = text;
+		this.fontType = fontType;
+	}
+	
+	public DTextModel()
+	{
+		super();
+		text = "Hello";
+		fontType = "Dialog.plain";
+	}
+	
+	public String getText()
+	{
+		return text;
+	}
+	
+	public void setText(String newText)
+	{
+		text = newText;
+	}
+	
+	public String getFontType()
+	{
+		return fontType;
+	}
+	
+	public void setFontType(String font)
+	{
+		fontType = font;
+	}
 
+	
 }
